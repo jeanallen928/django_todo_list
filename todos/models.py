e4f572895fb1e9ad9a836f257f76bf8b8eb90fcd
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-04-28 17:32:19 +0900

    create todo model and add to admin
    added Todo to todos/models.py
    registered Todo
    파이썬 장고 실무 심화 4주차 4강

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    is_complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    compition_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.title)
