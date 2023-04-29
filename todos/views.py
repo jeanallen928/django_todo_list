97501d3f61d2a765e9056f50f43eded24d231b31
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-04-29 16:08:28 +0900

    add update todo feature
    added update todo feature
    파이썬 장고 실무 심화 4주차 6강

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 from todos.models import Todo
 from todos.serializers import TodoSerializer, TodoCreateSerializer
 
@@ -15,19 +16,28 @@ class TodoView(APIView):
         serializer = TodoCreateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TodoDetailView(APIView):
     def get(self, request, todo_id):
-        pass
+        todo = get_object_or_404(Todo, id=todo_id)
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, todo_id):
-        pass
+        todo = get_object_or_404(Todo, id=todo_id)
+        serializer = TodoCreateSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, todo_id):
+        todo = get_object_or_404(Todo, id=todo_id)
         pass
 
 
